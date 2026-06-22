c2629ecc6b8b88fb673186d2076eac4290115156
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2026-06-21 22:57:23 -0700

    Optimized the approach to an expression body method that uses a chain of boolean expresions.

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -1,10 +1,4 @@
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-        if (year % 4 != 0)
-            return false;
-
-        return year % 100 == 0 ? year % 400 == 0 : true;
-    }
+    public static bool IsLeapYear(int year) => year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 }
