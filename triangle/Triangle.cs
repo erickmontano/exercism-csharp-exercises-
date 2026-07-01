f3f2626e8f742046e9a8a8fb6baf66f7309b0f71
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2026-07-01 13:40:01 -0700

    Completed the Triangle type exercise.
    Implemented IsScalene, IsIsosceles, and IsEquilateral methods in the Triangle class to determine triangle types based on side lengths. Added private helper methods IsTriangle for triangle validation and IsSideEqual for side comparison.

--- triangle/Triangle.cs
+++ triangle/Triangle.cs
@@ -1,17 +1,19 @@
 public static class Triangle
 {
-    public static bool IsScalene(double side1, double side2, double side3)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static bool IsScalene(double side1, double side2, double side3) => IsTriangle(side1, side2, side3)
+                                                                              && (!IsSideEqual(side1, side2)
+                                                                                  && !IsSideEqual(side1, side3)
+                                                                                  && !IsSideEqual(side2, side3));
 
-    public static bool IsIsosceles(double side1, double side2, double side3) 
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static bool IsIsosceles(double side1, double side2, double side3) => IsTriangle(side1, side2, side3)
+                                                                                && (IsSideEqual(side1, side2)
+                                                                                    || IsSideEqual(side1, side3)
+                                                                                    || IsSideEqual(side3, side2));
+    public static bool IsEquilateral(double side1, double side2, double side3) => IsTriangle(side1, side2, side3) && (IsSideEqual(side1, side2) && IsSideEqual(side1, side3));
 
-    public static bool IsEquilateral(double side1, double side2, double side3) 
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    private static bool IsTriangle(double side1, double side2, double side3) => (side1 > 0 && side2 > 0 && side3 > 0)
+                                                                               && (side1 + side2 >= side3)
+                                                                               && (side3 + side2 >= side1)
+                                                                               && (side1 + side3 >= side2);
+    private static bool IsSideEqual(double side1, double side2) => side1 == side2;
 }
