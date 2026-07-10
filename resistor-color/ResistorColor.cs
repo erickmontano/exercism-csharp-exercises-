9a9219dfb6c4636e66a364e1e0c7fa4f9f983d5a
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2026-07-09 19:16:39 -0700

    Initial setup for Resistor Color C# exercise
    Added solution stub, tests, project and config files for the "Resistor Color" exercise. Included .NET 10.0 project setup, xUnit test integration, and documentation (README, HELP). All necessary files for starting the exercise are present.

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,12 @@
+public static class ResistorColor
+{
+    public static int ColorCode(string color)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public static string[] Colors()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
