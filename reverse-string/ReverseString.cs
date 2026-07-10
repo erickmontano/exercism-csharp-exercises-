80aae1e0bd656f39d6497284d5a38ed775a24d40
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2026-07-09 18:06:37 -0700

    Refactor Reverse method using LINQ for conciseness
    Replaced the manual string reversal logic in ReverseString.Reverse with a LINQ-based implementation, improving code readability and conciseness.

--- reverse-string/ReverseString.cs
+++ reverse-string/ReverseString.cs
@@ -2,15 +2,5 @@ using System.Text;
 
 public static class ReverseString
 {
-    public static string Reverse(string input)
-    {
-        StringBuilder stringBuilder = new();
-
-        for (int i = input.Length - 1; i >= 0; i--)
-        {
-            stringBuilder.Append(input[i]);
-        }
-
-        return stringBuilder.ToString();
-    }
+    public static string Reverse(string input) => new string(input.Reverse().ToArray());
 }
