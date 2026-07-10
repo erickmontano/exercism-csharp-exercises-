d79f7676f1cf4ac5b8b0fea1cf014aa406cb1ea6
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2026-07-09 19:42:31 -0700

    Implement resistor color code logic and enable tests
    Added a static dictionary for resistor color values and implemented ColorCode and Colors methods. Enabled tests for White, Orange, and Colors by removing the Skip property.

--- resistor-color/ResistorColor.cs
+++ resistor-color/ResistorColor.cs
@@ -1,12 +1,20 @@
 public static class ResistorColor
 {
-    public static int ColorCode(string color)
+    private static readonly Dictionary<string, int> resisitorColorValues = new()
     {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+        {"black", 0 },
+        {"brown", 1},
+        {"red", 2},
+        {"orange", 3},
+        {"yellow", 4},
+        {"green", 5},
+        {"blue", 6},
+        {"violet", 7},
+        {"grey", 8},
+        {"white", 9},
+    };
 
-    public static string[] Colors()
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static int ColorCode(string color) => resisitorColorValues.GetValueOrDefault(color.ToLower());
+
+    public static string[] Colors() => resisitorColorValues.Keys.ToArray();
 }
