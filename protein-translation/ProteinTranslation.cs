799c855286c7dae9769bd5eafa90c386f09995bf
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2026-07-10 21:26:33 -0700

    Implement protein translation from RNA strand
    Added GetProteinStrand to map codons to proteins or "STOP". Updated Proteins method to process RNA strand, translate codons, halt at "STOP", and return protein names as an array.

--- protein-translation/ProteinTranslation.cs
+++ protein-translation/ProteinTranslation.cs
@@ -1,7 +1,37 @@
 public static class ProteinTranslation
 {
+    private static string? GetProteinStrand(string strand)
+    {
+        return strand switch
+        {
+            "AUG" => "Methionine",
+            "UUU" or "UUC" => "Phenylalanine",
+            "UUA" or "UUG" => "Leucine",
+            "UCU" or "UCC" or "UCA" or "UCG" => "Serine",
+            "UGU" or "UGC" => "Cysteine",
+            "UAU" or "UAC" => "Tyrosine",
+            "UGG" => "Tryptophan",
+            "UAA" or "UAG" or "UGA" => "STOP",
+            _ => null,
+        };
+    }
+
     public static string[] Proteins(string strand)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        List<string> result = new();
+
+        foreach (string s in strand.Chunk(3).Select(c => new string(c)))
+        {
+            string? protein = GetProteinStrand(s);
+
+            if (protein != null) 
+            {
+                if (protein == "STOP")
+                    break;
+
+                result.Add(protein);
+            }
+        }
+        return result.ToArray();
     }
 }
