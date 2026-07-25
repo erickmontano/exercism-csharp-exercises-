2f9691364ad983958ca49714d3530c2bed273af4
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2026-07-25 15:47:14 -0700

    Simplified Darts scoring to a single switch expression
    Removed the TargetArea enum and Dictionary<TargetArea, int> lookup in favor of a single switch expression mapping distance directly to
    score.
    
    All tests pass.

--- darts/Darts.cs
+++ darts/Darts.cs
@@ -1,31 +1,10 @@
 public static class Darts
 {
-    private enum TargetArea
+    public static int Score(double x, double y) => Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) switch
     {
-        InnerCircle, MiddleCircle, OuterCircle, Outside
-    }
-
-    private static readonly Dictionary<TargetArea, int> AreaValues = new()
-    {
-        {TargetArea.InnerCircle, 10},
-        {TargetArea.MiddleCircle, 5},
-        {TargetArea.OuterCircle, 1},
-        {TargetArea.Outside, 0},
+        <= 1 => 10,
+        <= 5 => 5,
+        <= 10 => 1,
+        _ => 0,
     };
-
-    private static TargetArea GetDartPosition(double x, double y)
-    {
-        switch (Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)))
-        {
-            case <= 1:
-                return TargetArea.InnerCircle;
-            case <= 5:
-                return TargetArea.MiddleCircle;
-            case <= 10:
-                return TargetArea.OuterCircle;
-            default:
-                return TargetArea.Outside;
-        }
-    }
-    public static int Score(double x, double y) => AreaValues[GetDartPosition(x, y)];
 }
